72317a607a841c592de44702c49b9cca5fe6e39c
Jacob Vallejo <you@jahkeup.com>
2014-03-28 19:18:11 -0400

    Add project dependencies and prepare speclj

--- project.clj
+++ project.clj
@@ -1,14 +1,21 @@
 (defproject job-board "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :description "Meade Job Board"
+  :url "http://board.meade-internal.com"
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2156"]
-                 [ring "1.2.1"]]
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [korma "0.3.0-RC5"]
+                 [org.postgresql/postgresql "9.2-1002-jdbc4"]]
   :plugins [[lein-cljsbuild "1.0.2"]
-            [lein-ring "0.8.10"]]
+            [lein-ring "0.8.10"]
+            [speclj "2.5.0"]]
+  :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
+  :test-paths ["spec"] ;; speclj testing path
   :hooks [leiningen.cljsbuild]
   :source-paths ["src/clj"]
-  :cljsbuild { 
+  :cljsbuild {
     :builds {
       :main {
         :source-paths ["src/cljs"]
