018112356db04702e59106659e2b9aafe82a98a3
Jacob Vallejo <you@jahkeup.com>
2014-04-04 00:42:18 -0400

    Add cache to system

--- src/clj/job_board/config/system.clj
+++ src/clj/job_board/config/system.clj
@@ -1,13 +1,17 @@
 (ns job-board.config.system
   (:require [job-board.components :refer :all]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]))
 
 (defn job-board-system [config]
   (let [{:keys [port slide-refresh slide-time]} config]
     (component/system-map
      :config config
      :webserver (new-webserver port)
-     :broadcaster (new-broadcaster slide-refresh slide-time))))
+     :broadcaster (new-broadcaster slide-refresh slide-time)
+     :cacheagent (new-cache-agent (env :exzigo-username)
+                                  (env :exzigo-password)
+                                  (env :exzigo-poll-interval)))))
 
 (def system (job-board-system {:port 3000 :slide-refresh 1
                                :slide-time 10}))
@@ -18,4 +22,3 @@
 (defn stop-system []
   (alter-var-root #'system component/stop))
 
-(start-system)
