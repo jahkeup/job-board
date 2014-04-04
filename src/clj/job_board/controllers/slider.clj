6f976ace1a5c38f1e4b7d88c6ff6b7ca623b609a
Jacob Vallejo <you@jahkeup.com>
2014-04-04 00:02:52 -0400

    Update slider controller to add it to broadcasts and set initial view

--- src/clj/job_board/controllers/slider.clj
+++ src/clj/job_board/controllers/slider.clj
@@ -2,14 +2,12 @@
   (:require [org.httpkit.server :refer :all]
             [job-board.views.slider :as view]
             [job-board.pages :refer [page-dump]]
-            [job-board.slider :refer [join-send-slide]]))
-
-
+            [job-board.slider :refer [join-send-slide drop-channel]]))
 
 (defn current [request]
   (with-channel request chn
-    (send! chn "hi")))
-
+    (on-close chn #(drop-channel chn))
+    (join-send-slide chn)))
 
 (defn slides []
   {:body (view/slides)
