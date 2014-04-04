03ae2ab6431f8b30af907b7a35cb720ba73c3dd6
Jacob Vallejo <you@jahkeup.com>
2014-04-04 11:35:54 -0400

    Revise system config loading process

--- src/clj/job_board/config/system.clj
+++ src/clj/job_board/config/system.clj
@@ -5,21 +5,28 @@
             [environ.core :refer [env]]))
 
 (defn job-board-system [config]
-  (let [{:keys [port slide-refresh slide-time]} config]
+  (let [{:keys [webserver-port slide-refresh slide-time
+                exzigo-username exzigo-password
+                exzigo-poll-interval]} config]
     (component/system-map
      :config config
-     :webserver (new-webserver (parse-int (env :webserver-port)))
-     :broadcaster (new-broadcaster slide-refresh slide-time)
-     :cacheagent (new-cache-agent (env :exzigo-username)
-                                  (env :exzigo-password)
-                                  (parse-int (env :exzigo-poll-interval))))))
 
-(def system (job-board-system {:port 3000 :slide-refresh 1
-                               :slide-time 10}))
+     :webserver (new-webserver webserver-port)
+     :broadcaster (new-broadcaster slide-time slide-refresh)
+     :cacheagent (new-cache-agent exzigo-username
+                                  exzigo-password
+                                  exzigo-poll-interval))))
 
+(def system (job-board-system {:webserver-port (or (parse-int (env :webserver-port)) 3000)
+
+                               :slide-refresh (or (parse-int (env :slide-refresh)) 1)
+                               :slide-time (or (parse-int (env :slide-time)) 10)
+
+                               :exzigo-username (env :exzigo-username)
+                               :exzigo-password (env :exzigo-password)
+                               :exzigo-poll-interval (parse-int (env :exzigo-poll-interval))}))
 (defn start-system []
   (alter-var-root #'system component/start))
 
 (defn stop-system []
   (alter-var-root #'system component/stop))
-
