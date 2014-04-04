816a89071ad3fd1d6f023b5ea86c9605b414e0f1
Jacob Vallejo <you@jahkeup.com>
2014-04-04 00:43:08 -0400

    And since when does the server need to do anything?

--- src/clj/job_board/server.clj
+++ src/clj/job_board/server.clj
@@ -1,18 +1,6 @@
 (ns job-board.server
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.resource :as resources]
-            [ring.util.response :as response]
-            [cheshire.core :as cheshire]
-            [compojure.handler :as handler]
-            [korma.core :refer :all]
-            [job-board.routes :as routes]
-            [job-board.authentication :as auth]
-            [job-board.database :refer [employees]]
-            [job-board.handlers :refer [app]])
+  (:require [job-board.config.system :refer [start-system]])
   (:gen-class))
 
-(defn start-server [join]
-  (jetty/run-jetty #'app {:port 3000 :join? join}))
-
 (defn -main [& args]
-  (prn "Starting application"))
+  (start-system))
